fa593f1ca4b58d9fb73d8c1add284b964fbeb790
Victor Osipov <victorosipov99@mail.ru>
2017-02-09 11:48:17 +0300

    add new script

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
-$(document).ready(function(){
-	$(".attractionList").owlCarousel({
-		items: 4,
-		margin: 26,
-		dots: true
-	});
-});
+function anichangeMen() {
+ if ( $(".jobs-active-men").css('display') == 'none' ) {
+ $(".jobs-active-men").animate({height: 'show'}, 400);
+ document.getElementById("jobs-arrow-men").style.transform = 'rotate(-180deg)';
+ } else {
+ $(".jobs-active-men").animate({height: 'hide'}, 200);
+ document.getElementById("jobs-arrow-men").style.transform = 'rotate(-360deg)';
+ }	
+}
+
+function anichangeUx() {
+ if ( $(".jobs-active-ux").css('display') == 'none' ) {
+ $(".jobs-active-ux").animate({height: 'show'}, 400);
+ document.getElementById("jobs-arrow-ux").style.transform = 'rotate(-180deg)';
+ } else {
+ $(".jobs-active-ux").animate({height: 'hide'}, 200);
+ document.getElementById("jobs-arrow-ux").style.transform = 'rotate(-360deg)';
+ }	
+}
+
+function anichangeFe() {
+ if ( $(".jobs-active-fe").css('display') == 'none' ) {
+ $(".jobs-active-fe").animate({height: 'show'}, 400);
+ document.getElementById("jobs-arrow-fe").style.transform = 'rotate(-180deg)';
+ } else {
+ $(".jobs-active-fe").animate({height: 'hide'}, 200);
+ document.getElementById("jobs-arrow-fe").style.transform = 'rotate(-360deg)';
+ }	
+}
+
+$(".tab-item").not(":first").hide();
+$(".tab").click(function() {
+	$(".tab").removeClass("active-panel-lk").eq($(this).index()).addClass("active-panel-lk");
+	$(".tab-item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active-panel-lk"); 
